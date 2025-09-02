b3bc9f12882fd4aae1ccd61b5f6619b5e9103aa1
Vu Nguyen <crisnguyendev@gmail.com>
2025-09-01 19:30:21 -0700

    Complete Bitwise Manipulation problems 832. Flipping an Image 136. Single Number 260. Single Number III

---
+++ dsa/src/dsa/bitwise/SingleNumber.kt
@@ -0,0 +1,16 @@
+package dsa.bitwise
+
+/**
+ * 136. Single Number
+ * Time: O(n)
+ * Space: O(1)
+ */
+class SingleNumber {
+    fun singleNumber(nums: IntArray): Int {
+        var result = 0
+        for (num in nums) {
+            result = result xor num
+        }
+        return result.toInt()
+    }
+}
