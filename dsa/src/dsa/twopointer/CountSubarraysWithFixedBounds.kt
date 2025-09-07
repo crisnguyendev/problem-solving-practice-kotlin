20d907c72d2d55928b0264dd438c4d40b1055fdd
Vu Nguyen <crisnguyendev@gmail.com>
2025-09-07 00:36:32 -0700

    Complete Two Pointers problems 2444. Count Subarrays With Fixed Bounds

--- dsa/src/dsa/twopointer/CountSubarraysWithFixedBounds.kt
+++ dsa/src/dsa/twopointer/CountSubarraysWithFixedBounds.kt
@@ -1,7 +1,34 @@
 package dsa.twopointer
 
+/**
+ * 2444. Count Subarrays With Fixed Bounds
+ * Time: O(n)
+ * Space: O(1)
+ */
 class CountSubarraysWithFixedBounds {
     fun countSubarrays(nums: IntArray, minK: Int, maxK: Int): Long {
-        return 0
+        var minPos = -1
+        var maxPos = -1
+        var windowStart = 0
+        var count = 0L
+        for (i in nums.indices) {
+            val num = nums[i]
+            if (num < minK || num > maxK) {
+                minPos = -1
+                maxPos = -1
+                windowStart = i + 1
+                continue
+            }
+            if (num == minK) {
+                minPos = i
+            }
+            if (num == maxK) {
+                maxPos = i
+            }
+            if (minPos != -1 && maxPos != -1) {
+                count += (minOf(minPos, maxPos) - windowStart + 1).toLong()
+            }
+        }
+        return count
     }
 }
